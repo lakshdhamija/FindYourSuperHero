e03f81ef7add127c3b580945ad941f58816e8961
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-07-18 13:30:37 +0530

    add basic structure

---
+++ js/common.js
@@ -0,0 +1,82 @@
+const Common = (function(){
+    const token = 10213278264309612;
+    const apiUrl = `https://www.superheroapi.com/api.php/${token}/`;
+    const loader = document.querySelector('.loader');
+    const notifContainer = document.querySelector('.notification');
+    const FAVARRAY = 'favs';
+
+    function showLoader(){
+        loader.style.display = 'block';
+    }
+    function hideLoader(){
+        loader.style.display = 'none';
+    }
+    function sendNotif(notif, type){ // funtion to show notifications
+        if(type === 'red'){
+            notifContainer.classList.remove('green');
+            notifContainer.classList.add('red');
+        }else if(type === 'green'){
+            notifContainer.classList.remove('red');
+            notifContainer.classList.add('green');
+        }
+        notifContainer.style.display = 'block';
+        notifContainer.textContent = notif;
+        setTimeout(() => { // remove container after 5s
+            notifContainer.style.display = 'none';
+        }, 5000);
+    }
+    function getFav(){
+        const heros = localStorage.getItem(FAVARRAY);
+        if(heros) return JSON.parse(heros);
+        else return [];
+    }
+    function addToFav(hero){
+        if(!hero) return;
+        const favHeroes = getFav(); // get array from local storage 
+        favHeroes.push(hero); // adding curr hero to array
+
+        localStorage.setItem(FAVARRAY, JSON.stringify(favHeroes)); // pushing updated array back to local storage
+    }
+    function removeFromFav(hero){
+        if(!hero) return;
+        let favHeroes = getFav();
+        favHeroes = favHeroes.filter((item) => {
+            item.id === hero
+        });
+
+        localStorage.setItem(FAVARRAY, JSON.stringify(favHeroes));
+    }
+
+    async function sendReq(url){ // send req to url
+        try{
+            const response = await fetch(url);
+            const data = await response.json();
+            return{
+                data, 
+                success:true
+            };
+        }catch(err){
+            console.log('error in fetching response: ', err);
+            return{
+                error: err.message,
+                success: false
+            };
+        }
+    }
+    function debounce(func, delay){
+        let timeout;
+        return  function(){
+            const context = this;
+            const args = arguements;
+            console.log('args', args);
+            clearTimeout(timeout);
+            timeout = setTimeout(function(){
+                timeout = null;
+                func.apply(context, args);
+            }, delay);
+        };
+    }
+    return{
+        sendReq, apiUrl, showLoader, hideLoader, sendNotif, getFav, addToFav, removeFromFav, debounce
+    }
+});
